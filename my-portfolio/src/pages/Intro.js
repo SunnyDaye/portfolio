bfb83e778eb9fcb7ac1940e2f971a8da3c4575ca
Suncerie <sunceried@gmail.com>
2022-12-29 17:41:10 -0800

    Homepage content stylized

--- my-portfolio/src/pages/Intro.js
+++ my-portfolio/src/pages/Intro.js
@@ -2,17 +2,27 @@ import React from "react";
 import './intro.css';
 function Intro() {
     return <div id="intro" className="page">
-        <div className="center">
-            <h1>Hi, my name is <span className="name">Suncerie Daye</span></h1>
-            <p>Learn more...</p>
-            <p>
-                <a href="#about">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                        <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
-                    </svg>
-                </a>
-
-            </p>
+        <div className="">
+            <div className="container">
+                <div className="Terminal">
+                    <div className="Terminal__Toolbar">
+                        <div className="Toolbar__buttons">
+                            <button className="Toolbar__button Toolbar__button--exit">&#10005;</button>
+                            <button className="Toolbar__button">&#9472;</button>
+                            <button className="Toolbar__button">&#9723;</button>
+                        </div>
+                        <p className="Toolbar__user">sunny@ubuntu:~</p>
+                    </div>
+                    <div className="Terminal__body">
+                        <div className="Terminal__text">Hi, my name is</div>
+                        <div className="Terminal__Prompt">
+                            <span className="Prompt__user">Suncerie Daye:</span><span className="Prompt__location">~</span><span className="Prompt__dollar">$</span>
+                            <span className="Prompt__cursor"></span>
+                        </div>
+                        <div className="Terminal__text">An aspiring software engineer learning to build performant and impactful applications.</div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
