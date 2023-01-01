2c34b81d8931723e7f036be705085739de26eacd
Suncerie <sunceried@gmail.com>
2023-01-01 10:46:46 -0800

    Contact page links added

--- my-portfolio/src/pages/Contact.js
+++ my-portfolio/src/pages/Contact.js
@@ -9,6 +9,8 @@ function Contact(){
             <label for="content">Content:<br/><textarea id="content" name="content" rows="25" cols="50" /></label><br/>
             <input type="submit" value="Send"/>
         </form>
+        <a href="https://github.com/SunnyDaye" target="_blank"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"/></a>
+        <a href="https://www.linkedin.com/in/sunny-daye/" target="_blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"/></a>
     </div>
 }
 
