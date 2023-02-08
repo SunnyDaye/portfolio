91022b91717eb6f866b1b593274c65d19ac8cc63
Suncerie <sunceried@gmail.com>
2023-02-07 19:02:17 -0800

    Ajax project added

--- my-portfolio/src/pages/Projects.js
+++ my-portfolio/src/pages/Projects.js
@@ -10,7 +10,7 @@ function Projects() {
                 <div className="slides-item slide-2" id="slide-2" aria-label="slide 2 of 5" tabIndex="0"><a href="https://sunnydaye.github.io/forms/" target="_blank">Click to view: Forms</a></div>
                 <div className="slides-item slide-3" id="slide-3" aria-label="slide 3 of 5" tabIndex="0"><a href="https://sunnydaye.github.io/data-processing/" target="_blank">Click to view: Data Processing</a></div>
                 <div className="slides-item slide-4" id="slide-4" aria-label="slide 4 of 5" tabIndex="0"><a href="https://sunnydaye.github.io/seasonal-styles/" target="_blank">Click to view: Seasonal Styles</a></div>
-                <div className="slides-item slide-5" id="slide-5" aria-label="slide 5 of 5" tabIndex="0">5</div>
+                <div className="slides-item slide-5" id="slide-5" aria-label="slide 5 of 5" tabIndex="0"><a href="https://ajax-random-quotes.sunnydaye.repl.co/ajax_headline2.htm" target="_blank">Click to view: AJAX Random Quotes</a></div>
             </div>
             <div className="carousel__nav">
                 <a className="slider-nav" href="#slide-1" aria-label="Go to slide 1">1</a>
